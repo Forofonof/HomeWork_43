8daabcc4d06de6c5e55ad0e9e2fe5fbd87ce9c8e
forofonof <DanielResult1@outlook.com>
2023-02-28 12:56:06 +0300

    FIX_1.1

--- HomeWork_43/Program.cs
+++ HomeWork_43/Program.cs
@@ -10,32 +10,47 @@ internal class Program
         const string From = "3";
         const string Send = "4";
         const string Info = "5";
+        const string Exit = "6";
+
+        bool isWork = true;
 
         TrainPlan plan = new TrainPlan();
 
         Console.WriteLine("Добро пожаловать! Что желаете сделать?");
 
-        while (true)
+        while (isWork)
         {
-            Console.WriteLine($"{Create} - Создать направление\n{Sell} - Продать билеты\n{From} - Количество вагонов\n{Send} - Отправка\n{Info} - План поездов");
+            Console.WriteLine($"{Create} - Создать направление\n{Sell} - Продать билеты" +
+                $"\n{From} - Количество вагонов\n{Send} - Отправка\n{Info} - План поездов\n{Exit} - Выход");
+
             string userInput = Console.ReadLine();
+
             switch (userInput)
             {
                 case Create:
                     plan.CreateDirection();
                     break;
+
                 case Sell:
                     plan.SellTickets();
                     break;
+
                 case From:
                     plan.FormTrain();
                     break;
+
                 case Send:
                     plan.SendTrain();
                     break;
+
                 case Info:
                     plan.PrintCurrentInfo();
                     break;
+
+                case Exit:
+                    isWork = false;
+                    break;
+
                 default:
                     Console.WriteLine("Ошибка! Нет такой команды :(");
                     break;
@@ -46,15 +61,16 @@ internal class Program
 
 class TrainPlan
 {
-    private string _destination;
-
+    private int minimumPassengerCount = 50;
+    private int maximumPassengerCount = 250;
+    private int minimumTrainCarCapacity = 50;
+    private int maximumTrainCarCapacity = 100;
     private int _numbersPassengers;
 
-    private int _trainCarCapacity;
+    private string _destination;
 
     private List<int> _train = new List<int>();
-
-    private Random rand = new Random();
+    private Random _random = new Random();
 
     public void CreateDirection()
     {
@@ -64,26 +80,27 @@ class TrainPlan
 
     public void SellTickets()
     {
-        _numbersPassengers = rand.Next(50, 250);
+        _numbersPassengers = _random.Next(minimumPassengerCount, maximumPassengerCount);
         Console.WriteLine($"Количество пассажиров: {_numbersPassengers}");
     }
 
-    public void CreateTrain()
+    public void CreatePlaces(out int trainCarCapacity)
     {
-        _trainCarCapacity = rand.Next(50, 100);
+        trainCarCapacity = _random.Next(minimumTrainCarCapacity, maximumTrainCarCapacity);
     }
 
     public void FormTrain()
     {
-        CreateTrain();
+        CreatePlaces(out int trainCarCapacity);
 
-        int numbersTrainCars = (int)Math.Ceiling((double)_numbersPassengers / _trainCarCapacity);
+        int countTrainCars = (int)Math.Ceiling((double)_numbersPassengers / trainCarCapacity);
 
-        for (int i = 0; i < numbersTrainCars; i++)
+        for (int i = 0; i < countTrainCars; i++)
         {
-            _train.Add(50);
+            _train.Add(0);
         }
-        Console.WriteLine($"Количество вагонов состава: {numbersTrainCars}\n Вместимость одного вагона: {_trainCarCapacity}");
+
+        Console.WriteLine($"Количество вагонов состава: {countTrainCars}\n Вместимость одного вагона: {trainCarCapacity}");
     }
 
     public void SendTrain()
